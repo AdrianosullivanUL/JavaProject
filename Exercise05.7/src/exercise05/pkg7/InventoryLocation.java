607013181f7952f3b539f8d0a981834ff6dbe0eb
Adrian-PC\Adrian <adrian.osullivan@piercom.ie>
2018-05-15 12:56:05 +0100

    Added inv loc classes

---
+++ Exercise05.7/src/exercise05/pkg7/InventoryLocation.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise05.pkg7;
+
+/**
+ * [Class Title Here]
+ *
+ * @author Adrian O'Sullivan Student ID 16230124 Last Modified [dd/mm/yyyy]
+ */
+class InventoryLocation {
+
+    private int inventoryLocationId;
+    private int section;
+    private int aisle;
+    private int rack;
+    private int shelf;
+    private int stockItemId;
+    private double quantity;
+
+    public InventoryLocation(int inventoryLocationId, int section, int aisle, int rack, int shelf) {
+        this.inventoryLocationId = inventoryLocationId;
+        this.section = section;
+        this.aisle = aisle;
+        this.rack = rack;
+        this.shelf = shelf;
+    }
+
+    public InventoryLocation(int inventoryLocationId, int section, int aisle, int rack, int shelf, int stockItemId, double quantity) {
+        this.inventoryLocationId = inventoryLocationId;
+        this.section = section;
+        this.aisle = aisle;
+        this.rack = rack;
+        this.shelf = shelf;
+        this.stockItemId = stockItemId;
+        this.quantity = quantity;
+    }
+
+    public int getInventoryLocationId() {
+        return inventoryLocationId;
+    }
+
+    public void setInventoryLocationId(int inventoryLocationId) {
+        this.inventoryLocationId = inventoryLocationId;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public void setSection(int section) {
+        this.section = section;
+    }
+
+    public int getAisle() {
+        return aisle;
+    }
+
+    public void setAisle(int aisle) {
+        this.aisle = aisle;
+    }
+
+    public int getRack() {
+        return rack;
+    }
+
+    public void setRack(int rack) {
+        this.rack = rack;
+    }
+
+    public int getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(int shelf) {
+        this.shelf = shelf;
+    }
+
+    public int getStockItemId() {
+        return stockItemId;
+    }
+
+    public void setStockItemId(int stockItemId) {
+        this.stockItemId = stockItemId;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+}
---
+++ Exercise05.7/src/exercise05/pkg7/InventoryLocationCollection.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercise05.pkg7;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+/**
+ * [Class Title Here]
+ * @author Adrian O'Sullivan
+ * Student ID 16230124
+ * Last Modified [dd/mm/yyyy]
+ */
+public class InventoryLocationCollection {
+    private List<InventoryLocation> iventoryStore;
+    
+    public void loadInventoryCollection()
+        throws FileNotFoundException
+    {
+        try
+        {
+            FileInputStream fis = new FileInputStream("iventoryStore.dat");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            List<InventoryLocation> inventoryStore = (List<InventoryLocation>)ois.readObject();
+            ois.close();
+            
+        }
+        catch (FileNotFoundException ex1)
+        {
+            
+        }
+        catch (IOException ex2)
+        {
+            
+        }
+        catch (ClassNotFoundException ex3)
+        {
+            
+        }
+        finally
+        {
+
+        }
+    }
+    public void saveInventoryCollection()
+        throws FileNotFoundException
+    {
+        try
+        {
+        
+            
+            FileOutputStream fos = new FileOutputStream("iventoryStore.dat");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(iventoryStore);
+            oos.close();
+        }
+        catch (FileNotFoundException ex1)
+        {
+            
+        }
+        catch (IOException ex2)
+        {
+            
+        }
+        finally
+        {
+
+        }
+    }
+    
+}
