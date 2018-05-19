c60ca7b1c8b85db76262ca306c434bc693524084
Adrian-PC\Adrian <adrian.osullivan@piercom.ie>
2018-05-19 08:28:11 +0100

    Finished Test harness for Inventory Item

--- Exercise05.7/src/exercise05/pkg7/InventoryLocationCollection.java
+++ Exercise05.7/src/exercise05/pkg7/InventoryLocationCollection.java
@@ -60,7 +60,6 @@ See InventoryLocation Test harness class
 
 
  */
-
 package exercise05.pkg7;
 
 import java.io.FileInputStream;
@@ -75,20 +74,22 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Class to persist a store or warehouse location/stock tracker, persistence is  
- * achieved though a simple serialised store in a locally stored file. 
- * Note: This is a single user solution.
+ * Class to persist a store or warehouse location/stock tracker, persistence is
+ * achieved though a simple serialised store in a locally stored file. Note:
+ * This is a single user solution.
  *
- * @author Adrian O'Sullivan Student ID 16230124 
+ * @author Adrian O'Sullivan Student ID 16230124
  */
 public class InventoryLocationCollection {
 
     private List<InventoryLocation> inventoryStore;
     private int currentEntryIndex = -1;
-/**
- * Constructor which loads the collection from file 
- * @throws ApplicationException 
- */
+
+    /**
+     * Constructor which loads the collection from file
+     *
+     * @throws ApplicationException
+     */
     public InventoryLocationCollection() throws ApplicationException {
         try {
             loadInventoryCollection();
@@ -96,10 +97,14 @@ public class InventoryLocationCollection {
             throw ex;
         }
     }
-/**
- * Load the inventory list from file and create a new list if it doesn't already exist
- * @throws ApplicationException to handle issues with accessing the file storage
- */
+
+    /**
+     * Load the inventory list from file and create a new list if it doesn't
+     * already exist
+     *
+     * @throws ApplicationException to handle issues with accessing the file
+     * storage
+     */
     public void loadInventoryCollection()
             throws ApplicationException {
         try {
@@ -121,11 +126,15 @@ public class InventoryLocationCollection {
 
         }
     }
-/**
- * Save any changes made to the file store
- * @param updatedInventoryLocationId WHen an inventory has been updated, this id is provided to support validation
- * @throws ApplicationException to handle issues with file access and also validation errors
- */
+
+    /**
+     * Save any changes made to the file store
+     *
+     * @param updatedInventoryLocationId WHen an inventory has been updated,
+     * this id is provided to support validation
+     * @throws ApplicationException to handle issues with file access and also
+     * validation errors
+     */
     void saveInventoryCollection(int updatedInventoryLocationId)
             throws ApplicationException {
         try {
@@ -145,10 +154,12 @@ public class InventoryLocationCollection {
 
         }
     }
-/**
- * Assign the next available identifier to the caller
- * @return the last saved id + 1
- */
+
+    /**
+     * Assign the next available identifier to the caller
+     *
+     * @return the last saved id + 1
+     */
     int getNextID() {
         int returnId = 0;
         if (inventoryStore != null && inventoryStore.size() > 0) {
@@ -159,18 +170,23 @@ public class InventoryLocationCollection {
         }
         return returnId;
     }
-/**
- * Get the number of locations stored
- * @return collection size
- */
+
+    /**
+     * Get the number of locations stored
+     *
+     * @return collection size
+     */
     int size() {
         return inventoryStore.size();
     }
-/**
- * Add a new inventory location
- * @param inventoryLocation pre-populated location
- * @throws ApplicationException to handle any issues with adding the new location
- */
+
+    /**
+     * Add a new inventory location
+     *
+     * @param inventoryLocation pre-populated location
+     * @throws ApplicationException to handle any issues with adding the new
+     * location
+     */
     void addInventoryLocation(InventoryLocation inventoryLocation)
             throws ApplicationException {
         for (InventoryLocation existingInventoryLocation : inventoryStore) {
@@ -183,11 +199,13 @@ public class InventoryLocationCollection {
         }
         inventoryStore.add(inventoryLocation);
     }
-/**
- * Validate changes made to the location entry
- * @param updateInventoryLocationId the identifier of the updated entry
- * @throws ApplicationException to handle issues with the update
- */
+
+    /**
+     * Validate changes made to the location entry
+     *
+     * @param updateInventoryLocationId the identifier of the updated entry
+     * @throws ApplicationException to handle issues with the update
+     */
     private void validateUpdates(int updateInventoryLocationId)
             throws ApplicationException {
 
@@ -215,47 +233,55 @@ public class InventoryLocationCollection {
             throw new ApplicationException("You cannot have a stock without selecting a stock item");
         }
     }
-/**
- * Delete an inventory item
- * @param inventoryLocationId The identifier of the location to delete
- * @throws ApplicationException issus with doing the delete
- */
+    /**
+     * Delete an inventory item
+     *
+     * @param inventoryLocationId The identifier of the location to delete
+     * @throws ApplicationException issus with doing the delete
+     */
     void deleteInventoryLocation(int inventoryLocationId) throws ApplicationException {
-        boolean deleted = true;
         for (int i = 0; i <= inventoryStore.size() - 1; i++) {
-            if (Integer.compare(inventoryLocationId, inventoryStore.get(i).getInventoryLocationId()) == 0) {
+            //if (Integer.compare(inventoryLocationId, inventoryStore.get(i).getInventoryLocationId()) == 0) {
+            if (inventoryStore.get(i).getInventoryLocationId() == inventoryLocationId ) {
                 inventoryStore.remove(i);
-                deleted = true;
-            }
-            if (deleted == false) {
-                throw new ApplicationException("Entry not deleted");
+                return;
             }
         }
+        throw new ApplicationException("Entry not deleted");
     }
+
 /**
- * return the current inventory item that the current inventory index is pointing to
+ * return the current inventory item that the current inventory index is
+ * pointing to
+ *
  * @return the current inventory item
  */
-    public InventoryLocation getCurrentInventoryLocation() {
+public InventoryLocation getCurrentInventoryLocation() {
         InventoryLocation inventoryLocation = null;
         if (currentEntryIndex > -1 && currentEntryIndex <= inventoryStore.size() - 1) {
             inventoryLocation = inventoryStore.get(currentEntryIndex);
         }
         return inventoryLocation;
     }
-/**
- * Move to the position at the head of the list, this positions the indexer to support iterating through the list
- */
+
+    /**
+     * Move to the position at the head of the list, this positions the indexer
+     * to support iterating through the list
+     */
     public void moveToHeadLocation() {
         currentEntryIndex = -1;
     }
-        public void moveToTailLocation() {
+
+    public void moveToTailLocation() {
         currentEntryIndex = inventoryStore.size(); // Note: forced to be 1 greater than the index location
     }
-/**
- * Move to the next inventory location, supports iterating through each item in the list
- * @return true if the next location exists, false if the end of the list 
- */
+
+    /**
+     * Move to the next inventory location, supports iterating through each item
+     * in the list
+     *
+     * @return true if the next location exists, false if the end of the list
+     */
     public boolean moveToNextInventoryLocation() {
         boolean returnValue = true;
 
@@ -266,10 +292,14 @@ public class InventoryLocationCollection {
         }
         return returnValue;
     }
-/**
- * Move to the previous inventory location, supports iterating through each item in the list
- * @return true if the previous location exists, false if the end of the list
- */
+
+    /**
+     * Move to the previous inventory location, supports iterating through each
+     * item in the list
+     *
+     * @return true if the previous location exists, false if the end of the
+     * list
+     */
     boolean moveToPreviousInventoryLocation() {
         boolean returnValue = true;
 
@@ -280,12 +310,15 @@ public class InventoryLocationCollection {
         }
         return returnValue;
     }
-/**
- * Check and see if the location add/edit will result in a duplicated location
- * @param existing the existing location
- * @param updated the location being updated
- * @return true if the result will cause duplication, false if not
- */
+
+    /**
+     * Check and see if the location add/edit will result in a duplicated
+     * location
+     *
+     * @param existing the existing location
+     * @param updated the location being updated
+     * @return true if the result will cause duplication, false if not
+     */
     private boolean checkForDuplicateLocation(InventoryLocation existing, InventoryLocation updated) {
         boolean result = false;
         if (existing.getSection() == updated.getSection()
@@ -296,10 +329,12 @@ public class InventoryLocationCollection {
         }
         return result;
     }
-/**
- * Sort the list by a pre-defined key
- * @param sortKey which attribute to sort by
- */
+
+    /**
+     * Sort the list by a pre-defined key
+     *
+     * @param sortKey which attribute to sort by
+     */
     public void sortBy(String sortKey) {
         if (sortKey == "Section") {
             Collections.sort(inventoryStore, new InventoryLocationSectionComparator());
@@ -309,25 +344,29 @@ public class InventoryLocationCollection {
             Collections.sort(inventoryStore, new InventoryLocationIdComparator());
             moveToHeadLocation();
 
-        }
+        
+
+}
     }
-/**
- * Define a comparator to support sorting by Section
- */
+
+    /**
+     * Define a comparator to support sorting by Section
+     */
     private class InventoryLocationSectionComparator implements Comparator<InventoryLocation> {
 
-        public int compare(InventoryLocation self, InventoryLocation other) {
+    public int compare(InventoryLocation self, InventoryLocation other) {
 
-            return Integer.valueOf(self.getSection()).compareTo(other.getSection());
-        }
+        return Integer.valueOf(self.getSection()).compareTo(other.getSection());
     }
+}
+
 /**
  * Define a comparator to sort by entry sequence (id)
  */
-    private class InventoryLocationIdComparator implements Comparator<InventoryLocation> {
+private class InventoryLocationIdComparator implements Comparator<InventoryLocation> {
 
-        public int compare(InventoryLocation self, InventoryLocation other) {
-            return Integer.valueOf(self.getInventoryLocationId()).compareTo(other.getInventoryLocationId());
-        }
+    public int compare(InventoryLocation self, InventoryLocation other) {
+        return Integer.valueOf(self.getInventoryLocationId()).compareTo(other.getInventoryLocationId());
     }
+}
 }
--- Exercise05.7/src/exercise05/pkg7/InventoryLocationTestHarness.java
+++ Exercise05.7/src/exercise05/pkg7/InventoryLocationTestHarness.java
@@ -51,6 +51,7 @@ class InventoryLocationTestHarness {
             int inventoryCount = inventoryCollection.size();
 
             // add 5 valid rows
+            System.out.println("Test #1 - Add 5 valid rows");
             AddNewLocationTest(true, "Add valid record", new InventoryLocation(9990, 1, 6, 11, 0));
             AddNewLocationTest(true, "Add valid record", new InventoryLocation(9991, 2, 7, 12, 0));
             AddNewLocationTest(true, "Add valid record", new InventoryLocation(9992, 3, 8, 13, 0));
@@ -61,17 +62,21 @@ class InventoryLocationTestHarness {
             }
 
             // Add a duplicate row
+            System.out.println("Test #2 - Add a duplicated row");
             AddNewLocationTest(false, "Add a duplicate row", new InventoryLocation(9994, 5, 10, 15, 0));
 
             // delete a row that doesnt exist
+            System.out.println("Test #3 - Try to delete a rown that does not exist");
             DeleteLocationTest(false, "Delete row that doesnt exist ", 9995);
 
             // Update a row
+            System.out.println("Test #4 - Try to set a quantity with no part");
             InventoryLocation inventoryLocation = GetLocation(9990);
             inventoryLocation.setQuantity(1);
             UpdateLocationTest(false, "Update qty with no part", inventoryLocation.getInventoryLocationId());
 
             // Delete all test rows
+            System.out.println("Test #5 - Delete 5 test rows");
             DeleteLocationTest(true, "Delete row ", 9990);
             DeleteLocationTest(true, "Delete row ", 9991);
             DeleteLocationTest(true, "Delete row ", 9992);
@@ -90,23 +95,35 @@ class InventoryLocationTestHarness {
 
         try {
             inventoryCollection.addInventoryLocation(inventoryLocation);
-            System.out.println("Pass: Test Description " + testDescription + " Added:" + inventoryLocation.toString());
+            if (ExpectPassOutcome == true) {
+                System.out.println("Pass: Test Description " + testDescription + " Added:" + inventoryLocation.toString());
+            } else {
+                System.out.println("**TEST FAIL: Test Description " + testDescription + " Added:" + inventoryLocation.toString());
+            }
         } catch (ApplicationException ex) {
-            if (ExpectPassOutcome) {
-                System.out.println("**TEST FAIL: Application Exception " + ex.getMessage() + " : " + inventoryLocation.toString());
+            if (ExpectPassOutcome == false) {
+                System.out.println("Pass: Application Exception " + ex.getMessage() + " : " + inventoryLocation.toString());
+            } else {
+                System.out.println("APPLICATION FAIL " + ex.getMessage());
             }
         }
-
     }
 
     private void DeleteLocationTest(boolean ExpectPassOutcome, String testDescription, int inventoryLocationId) throws ApplicationException {
         try {
             inventoryCollection.deleteInventoryLocation(inventoryLocationId);
-            System.out.println("Pass: Test Description " + testDescription + " Deleted Id: " + inventoryLocationId);
-        } catch (ApplicationException ex) {
-            if (ExpectPassOutcome) {
-                System.out.println("**TEST FAIL: Application Exception " + ex.getMessage() + " Id: " + inventoryLocationId);
+            if (ExpectPassOutcome == true) {
+                System.out.println("Pass: Test Description " + testDescription + " Deleted Id: " + inventoryLocationId);
+            } else {
+                System.out.println("**TEST FAIL: Test Description " + testDescription + " Deleted Id: " + inventoryLocationId);
             }
+        } catch (ApplicationException ex) {
+            if (ExpectPassOutcome == false) {
+                System.out.println("Pass: Application Exception " + ex.getMessage() + " Id: " + inventoryLocationId);
+            } else {
+                System.out.println("APPLICATION FAIL " + ex.getMessage());
+            }
+
         }
     }
 
@@ -114,11 +131,17 @@ class InventoryLocationTestHarness {
         try {
 
             inventoryCollection.saveInventoryCollection(inventoryLocationId);
-            System.out.println("Pass: Test Description " + testDescription + " Result Id:" + inventoryLocationId);
+            if (ExpectPassOutcome == true) {
+                System.out.println("Pass: Test Description " + testDescription + " Result Id:" + inventoryLocationId);
+            } else {
+                System.out.println("**TEST FAIL: Test Description " + testDescription + " Result Id:" + inventoryLocationId);
+            }
 
         } catch (ApplicationException ex) {
-            if (ExpectPassOutcome) {
-                System.out.println("**TEST FAIL: Application Exception " + ex.getMessage() + " Id:" + inventoryLocationId);
+            if (ExpectPassOutcome == false) {
+                System.out.println("Pass: Application Exception " + ex.getMessage() + " Id:" + inventoryLocationId);
+            } else {
+                System.out.println("**APPLICATION FAIL " + ex.getMessage());
             }
         }
     }
@@ -126,13 +149,13 @@ class InventoryLocationTestHarness {
     private InventoryLocation GetLocation(int InventoryLocationId) throws ApplicationException {
         InventoryLocation inventoryLocation = null;
 
-            inventoryCollection.moveToHeadLocation();
-            while (inventoryCollection.moveToNextInventoryLocation()) {
-                if (Integer.compare(InventoryLocationId, inventoryCollection.getCurrentInventoryLocation().getInventoryLocationId()) == 0) {
-                    inventoryLocation = inventoryCollection.getCurrentInventoryLocation();
-                    break;
-                }
+        inventoryCollection.moveToHeadLocation();
+        while (inventoryCollection.moveToNextInventoryLocation()) {
+            if (Integer.compare(InventoryLocationId, inventoryCollection.getCurrentInventoryLocation().getInventoryLocationId()) == 0) {
+                inventoryLocation = inventoryCollection.getCurrentInventoryLocation();
+                break;
             }
-            return inventoryLocation;
+        }
+        return inventoryLocation;
     }
 }
